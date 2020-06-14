4a446957e3f49867b1c8173c7a3641cd8423a362
Rick Ucker <rick.ucker@gmail.com>
2020-06-14 16:53:09 -0400

    Extract document to test constant, add additional props

--- test/delicious_parser_test.exs
+++ test/delicious_parser_test.exs
@@ -1,22 +1,23 @@
 defmodule DeliciousParserTest do
   use ExUnit.Case
 
+  @document """
+  <!DOCTYPE NETSCAPE-Bookmark-file-1>
+  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+  <!-- This is an automatically generated file.
+  It will be read and overwritten.
+  Do Not Edit! -->
+  <TITLE>Bookmarks</TITLE>
+  <H1>Bookmarks</H1>
+  <DL><p>
+  <DT><A href="http://some-url.org" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\">Hey, a description!</A>
+  <DD>Some comments
+  """
+
   doctest DeliciousParser
 
   test "filters html elements matching <DT> or <DD>" do
-    document = """
-    <!DOCTYPE NETSCAPE-Bookmark-file-1>
-    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-    <!-- This is an automatically generated file.
-    It will be read and overwritten.
-    Do Not Edit! -->
-    <TITLE>Bookmarks</TITLE>
-    <H1>Bookmarks</H1>
-    <DL><p>
-    <DT><A href="http://some-url.org"</A>
-    <DD>Hey, a description!
-    """
-    assert DeliciousParser.filter_elements(document) == [ ~s|<DT><A href="http://some-url.org"</A>|, ~s|<DD>Hey, a description!| ]
+    assert DeliciousParser.filter_elements(@document) == [ ~s|<DT><A href="http://some-url.org" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\">Hey, a description!</A>|, ~s|<DD>Some comments| ]
 	end
 
   test "strips markup from lines, leaving only properties and values" do
