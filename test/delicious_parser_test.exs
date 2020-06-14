f73cd459fd53e7eea6b1b34c3e7229cfb99bc402
Rick Ucker <rick.ucker@gmail.com>
2020-06-14 17:42:06 -0400

    Add second link to document

--- test/delicious_parser_test.exs
+++ test/delicious_parser_test.exs
@@ -12,12 +12,13 @@ defmodule DeliciousParserTest do
   <DL><p>
   <DT><A href="http://some-url.org" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\">Hey, a description!</A>
   <DD>Some comments
+  <DT><A href="http://another-url.org" ADD_DATE=\"1486993837\" PRIVATE=\"0\" TAGS=\"bar,baz\">About the turbo encabulator</A>
   """
 
   doctest DeliciousParser
 
   test "filters html elements matching <DT> or <DD>" do
-    assert DeliciousParser.filter_elements(@document) == [ ~s|<DT><A href="http://some-url.org" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\">Hey, a description!</A>|, ~s|<DD>Some comments| ]
+    assert DeliciousParser.filter_elements(@document) == [ ~s|<DT><A href="http://some-url.org" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\">Hey, a description!</A>|, ~s|<DD>Some comments|, ~s|<DT><A href="http://another-url.org" ADD_DATE=\"1486993837\" PRIVATE=\"0\" TAGS=\"bar,baz\">About the turbo encabulator</A>| ]
 	end
 
   test "strips markup from lines, leaving only properties and values" do
