d06262c59262f475da0da777e8d853dc405ffbbc
Rick Ucker <rick.ucker@gmail.com>
2020-06-22 13:07:40 -0400

    Extract test values to constants

--- test/delicious_parser_test.exs
+++ test/delicious_parser_test.exs
@@ -16,6 +16,32 @@ defmodule DeliciousParserTest do
   </DL><p>
   """
 
+  @link_with_comments "href=\"http://some-url.org\" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\" TITLE=\"Link title\" COMMENTS=\"Some comments\""
+  @simple_link "href=\"http://another-url.org\" ADD_DATE=\"1486993837\" PRIVATE=\"0\" TAGS=\"bar,baz\" TITLE=\"About the turbo encabulator\" "
+  @link_with_urlparams "href=\"http://url-with-params.org/?p1=foo&p2=bar\" ADD_DATE=\"1486993841\" PRIVATE=\"0\" TAGS=\"qux\" TITLE=\"This is a tricksy link\" "
+
+  @link_with_comments_map %{
+    href: "http://some-url.org",
+    add_date: "1498938954",
+    private: "1",
+    tags: ["foo"],
+    comments: "Some comments",
+    title: "Link title"
+  }
+  @simple_link_map %{
+    href: "http://another-url.org",
+    add_date: "1486993837",
+    private: "0",
+    title: "About the turbo encabulator",
+    tags: ["bar,baz"]
+  }
+  @link_with_urlparams_map %{
+    href: "http://url-with-params.org/?p1=foo&p2=bar",
+    add_date: "1486993841",
+    private: "0",
+    tags: ["qux"],
+    title: "This is a tricksy link"
+  }
   doctest DeliciousParser
 
   test "filters html elements matching <DT> or <DD>" do
@@ -30,67 +56,37 @@ defmodule DeliciousParserTest do
     input = DeliciousParser.filter_elements(@document)
 
     assert DeliciousParser.strip_markup(input) == [
-             "href=\"http://some-url.org\" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\" TITLE=\"Link title\" COMMENTS=\"Some comments\"",
-             "href=\"http://another-url.org\" ADD_DATE=\"1486993837\" PRIVATE=\"0\" TAGS=\"bar,baz\" TITLE=\"About the turbo encabulator\" "
+             @link_with_comments,
+             @simple_link
            ]
   end
 
   test "maps link properties" do
     input = [
-      "href=\"http://some-url.org\" ADD_DATE=\"1498938954\" PRIVATE=\"1\" TAGS=\"foo\" TITLE=\"Link title\" COMMENTS=\"Some comments\"",
-      "href=\"http://another-url.org\" ADD_DATE=\"1486993837\" PRIVATE=\"0\" TAGS=\"bar,baz\" TITLE=\"About the turbo encabulator\" ",
-      "href=\"http://url-with-params.org/?p1=foo&p2=bar\" ADD_DATE=\"1486993841\" PRIVATE=\"0\" TAGS=\"qux\" TITLE=\"This is a tricksy link\" "
+      @simple_link,
+      @link_with_comments,
+      @link_with_urlparams
     ]
 
     assert DeliciousParser.map_links(input) == [
-             %{
-               href: "http://some-url.org",
-               add_date: "1498938954",
-               private: "1",
-               tags: ["foo"],
-               comments: "Some comments",
-               title: "Link title"
-             },
-             %{
-               href: "http://another-url.org",
-               add_date: "1486993837",
-               private: "0",
-               title: "About the turbo encabulator",
-               tags: ["bar,baz"]
-             },
-             %{
-               href: "http://url-with-params.org/?p1=foo&p2=bar",
-               add_date: "1486993841",
-               private: "0",
-               tags: ["qux"],
-               title: "This is a tricksy link"
-             }
+             @simple_link_map,
+             @link_with_comments_map,
+             @link_with_urlparams_map
            ]
   end
 
   test "encodes links to CSV" do
     input = [
-      %{
-        href: "http://some-url.org",
-        add_date: "1498938954",
-        private: "1",
-        tags: ["foo"],
-        comments: "Some comments",
-        title: "Link title"
-      },
-      %{
-        href: "http://another-url.org",
-        add_date: "1486993837",
-        private: "0",
-        title: "About the turbo encabulator",
-        tags: ["bar,baz"]
-      }
+      @link_with_comments_map,
+      @simple_link_map,
+      @link_with_urlparams_map
     ]
 
     assert DeliciousParser.encode_csv(input) == [
              "href,title,add_date,private,comments,tags\r\n",
              "http://some-url.org,Link title,1498938954,1,Some comments,foo\r\n",
-             "http://another-url.org,About the turbo encabulator,1486993837,0,,\"bar,baz\"\r\n"
+             "http://another-url.org,About the turbo encabulator,1486993837,0,,\"bar,baz\"\r\n",
+             "http://url-with-params.org/?p1=foo&p2=bar,This is a tricksy link,1486993841,0,,qux\r\n"
            ]
   end
 end
